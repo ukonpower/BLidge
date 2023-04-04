8582007707294c1283a1eed86b0c08578a9be159
ukonpower <ore.ukonpower@gmail.com>
2023-04-05 01:12:20 +0900

    object visibility

--- blidge/utils/scene_parser.py
+++ blidge/utils/scene_parser.py
@@ -102,7 +102,8 @@ class SceneParser:
             "material": {
                 "name": "",
                 "uniforms": {}
-            }
+            },
+            "visible": not object.hide_render
         }
 
         # animation
@@ -272,6 +273,7 @@ class SceneParser:
             "rotation": { "x": 0.0, "y": 0.0, "z": 0.0 },
             "scale": { "x": 1.0, "y": 1.0, "z": 1.0 },
             "type": 'empty',
+            "visible": True,
             "material": {
                 "name": "",
                 "uniforms": []
