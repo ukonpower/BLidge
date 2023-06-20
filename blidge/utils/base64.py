23b75dcc8f3ed1f82594e4903e18f6ab7a73ba17
ukonpower <ore.ukonpower@gmail.com>
2023-06-20 18:24:19 +0900

    mesh param

--- blidge/utils/base64.py
+++ blidge/utils/base64.py
@@ -2,7 +2,7 @@ import base64
 import struct
 
 def int_array_to_base64( array ):
-    return base64.b64encode(b''.join([struct.pack('<I', v) for v in array])) 
+    return base64.b64encode(b''.join([struct.pack('<H', v) for v in array])).decode("ascii")
 
 def float_array_to_base64( array ):
-    return base64.b64encode(b''.join([struct.pack('<f', v) for v in array]))
+    return base64.b64encode(b''.join([struct.pack('<f', v) for v in array])).decode("ascii")
